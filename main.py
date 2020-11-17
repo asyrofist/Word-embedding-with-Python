0ae2db0515dd4a585f19d9072c33afcc808e86b1
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-17 14:41:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import nltk
 from nltk.corpus import gutenberg
 from gensim.models import Word2Vec
+from gensim.models import word2vec
 from gensim.models import KeyedVectors
 from multiprocessing import Pool
 from scipy import spatial
@@ -59,7 +60,7 @@ elif  dataset == 'wiki8':
     # preprocess
     col1, col2 = st.beta_columns([3,1])
     col1.subheader("Dataset")
-    sentences = Word2Vec.Text8Corpus('enwik8')
+    sentences = word2vec.Text8Corpus('enwik8')
     col1.dataframe(sentences)
 
     # vocabulary
@@ -70,8 +71,7 @@ elif  dataset == 'wiki8':
 
     # Model    
     filename = 'GoogleNews-vectors-negative300.bin.gz'
-    model = Word2Vec(sentences, size=200, hs=1)
-    model.wv.most_similar(positive=['woman', 'king'], negative=['man'], topn=1)
+    model = word2vec.Word2Vec(sentences, size=200, hs=1)
     kata_value = st.selectbox('What mode?',kata)
     hasil = model.most_similar(kata_value)
     st.dataframe(hasil)
